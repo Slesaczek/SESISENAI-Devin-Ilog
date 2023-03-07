25f3518b10eb908c069a882e2c3fc2e367deaf03
Slesaczek <cesarslesac@hotmail.com>
2023-03-06 23:04:54 -0300

    Adicionado página Home

--- Módulo 2/M02S07/Exercícios/plataformaCursos/src/App.jsx
+++ Módulo 2/M02S07/Exercícios/plataformaCursos/src/App.jsx
@@ -1,7 +1,9 @@
+import Home from "./pages/Home"
+
 function App() {
   return (
-    <div className="App">
-      <h1>DevInCursos</h1>
+    <div className="App">   
+      <Home/>   
     </div>
   )
 }
---
+++ Módulo 2/M02S07/Exercícios/plataformaCursos/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar"
+
+function Home() {
+  return (
+    <div className="App">   
+      <Navbar/>   
+    </div>
+  )
+}
+
+export default Home;
